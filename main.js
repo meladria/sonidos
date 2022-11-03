c79f712e005d0f0e2d7b9e73e05ca69ba25f8c06
meladria <98776699+meladria@users.noreply.github.com>
2022-11-02 21:06:58 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -15,5 +15,32 @@ function gotResults(error, results) {
     
     console.log(results);
   }
-  
-}
+  random_number_r = Math.floor(Math.random() * 255) + 1;
+  random_number_g = Math.floor(Math.random() * 255) + 1;
+  random_number_b = Math.floor(Math.random() * 255) + 1;
+
+  document.getElementById("result_label").innerHTML = 'Escucho:  '+ results[0].label;
+  document.getElementById("result_confidence").innerHTML = 'Presición:  '+ (results[0].confidence*100).toFixed(2)+" %";
+  document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+  document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    var images = [
+      "perroladra.gif",
+      "gato.gif",
+      "vaca1.gif",
+      "tigre.gif"
+    ];
+
+    
+    if (results[0].label == "ladridos") {
+     document.getElementById("imagen_online").src = images[0];
+    } else if (results[0].label == "maullidos") {
+      document.getElementById("imagen_online").src = images[1];
+    } else if (results[0].label == "mujidos") {
+      document.getElementById("imagen_online").src = images[2];
+    }else{
+      document.getElementById("imagen_online").src = images[3];
+    }
+  }
+
+
